bd01cb7a59b15f55275d6b10d9f0fffe19d435a6
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-08-08 00:15:26 +0100

    active element button styling

--- app.js
+++ app.js
@@ -1,60 +1,5 @@
 //data filtering 
 
-/*
-let btn = document.querySelectorAll('#category');
-let names = '';
-for (var i = 0; i < btn.length; i++) {
-    names += btn[i].value;
-}
-
-console.log(names)
-
-btn.addEventListener("click", function (e) {
-    
-    let results = document.getElementsById('results');
-
-    if ([...document.querySelectorAll('[value = frontend]')]) {
-        results.innerHTML = e.target.dataset.id;
-    } 
-    
-})
-
-
-    let frontend = document.querySelectorAll('[value=frontend]').forEach(element => {
-        console.log(element)
-    })
-    
-    let senior = document.querySelectorAll('[value = senior]').forEach(element => {
-        element.addEventListener('click', function() {
-            element.classlist.toggle('new')
-        } )
-    })
-
-*/
-
-
-
-
-
-
-
-/*
-
-
-document.addEventListener('click', function() {
-    let btn = document.querySelectorAll('#category');
-    btn.value;
-    console.log(btn.value);
-    console.log(btn)
-    for(let i = 0; i < btn.length; i++) {
-        let element = btn.getAttribute('value');
-    console.log(element)
-    }
-    
-})
-*/
-
-
 const frontend = [...document.querySelectorAll('[value = frontend]')];
 const senior = [...document.querySelectorAll('[value = senior]')];
 const html = [...document.querySelectorAll('[value = html]')];
@@ -73,26 +18,10 @@ arr.forEach(element => {
     element.addEventListener('click', function (event) {
         event.stopPropagation();
         event.preventDefault()
-        if (senior) {
+
+        if (element) {
             results.innerHTML +=
                 `
-            <div class="element-item">
-                <span>${element.value}</span>
-                <span class="material-icons close-icon">close</span>
-            </div>
-             `;
-            console.log(element)
-        } else if (frontend) {
-            results.innerHTML +=
-                `
-            <div class="element-item">
-                <span>${element.value}</span>
-                <span class="material-icons close-icon">close</span>
-            </div>
-             `;
-            console.log(element)
-        } else if (html) {
-            results.innerHTML += `
             <div class="element-item">
                 <span>${element.value}</span>
                 <span class="material-icons close-icon">close</span>
@@ -114,88 +43,4 @@ arr.forEach(element => {
             }
         };
     })
-
-
-
-
-
-
-
-
-    /*
-    
-    document.addEventListener('click', function () {
-        let results = document.getElementById('results');
-        if (arr.value = 'senior') {
-            results.innerHTML = '<div><p>Senior</p></div>';
-            console.log(arr.value)
-        } else if (arr.value = "frontend") {
-            results.innerHTML = arr.value;
-            results.innerHTML = '<div><p>Frontend</p></div>';
-            console.log(arr.value)
-        } else if (arr.value = "html") {
-            results.innerHTML = arr.value;
-            console.log(arr.value)
-        }
-    
-        document.addEventListener('click', function(event) {
-                event.preventDefault()
-                let searchItem = document.querySelectorAll('.element-item');
-                let closeButton = document.querySelectorAll('.close-icon')
-                searchItem.forEach(element => {
-                    this.style.display = "none"
-                })
-            })
-    })
-    */
-
-
-
-
-
-    /*arr.addEventListener("click", function (e) {
-        
-        let results = document.getElementsById('results');
-    
-        if ([...document.querySelectorAll('[value = frontend]')]) {
-            results.innerHTML = e.target.dataset.id;
-        } 
-        
-    })*/
 })
-
-
-
-
-
-
-/*
-
-console.log(items)
-
-items.addEventListener('click', function(e) {
-    document.getElementById('results').innerHTML = e.target.dataset.id;
-})
-
-
-/*
-
-let results = $("#category").map(function() {
-    return $(this).data("id");
-}).get();
-console.log(results);
-
-let items = document.querySelector(`[data-id]`);
-
-let modifiedItems = items.map(function(element) {
-    return element;
-})
-
-console.log(modifiedItems);
-
-console.log(items)
-
-items.addEventListener('click', function(e) {
-    document.getElementById('results').innerHTML = e.target.dataset.id;
-})
-*/
