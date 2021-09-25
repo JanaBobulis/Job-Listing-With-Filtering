8b5d1211c08a7bbd834425ab71458c3ef2da16e1
Jana Bobulis <jana.bobulis@hotmail.com>
2021-09-25 18:35:00 +0100

    onclick

--- app.js
+++ app.js
@@ -1,81 +1,91 @@
-//data filtering 
+//data filtering
 
-const frontend = [...document.querySelectorAll('[value = frontend]')];
-console.log(frontend)
-// const html = [...document.querySelectorAll('[value = html]')];
-
-
-
-const itemValues = [...document.querySelectorAll('.selector')];
-const itemValues3 = Array.from(document.querySelectorAll('[value]'))
-const itemValues4 = [...document.querySelectorAll('[value]')].map(value => value.innerHTML)
-
+const frontend = [...document.querySelectorAll("[value = frontend]")];
+const itemValues = [...document.querySelectorAll(".selector")];
+const itemValues3 = Array.from(document.querySelectorAll("[value]"));
+const itemValues4 = [...document.querySelectorAll("[value]")].map(
+  (value) => value.innerHTML
+);
 
 //filters all elements in an array and deleted dublicates
 let uniqueArrayElement = itemValues4.filter(function (element, index, self) {
-    return index === self.indexOf(element);
-})
+  return index === self.indexOf(element);
+});
 
-itemValues.map(element => {
-    // console.log(element)
-    let results = document.querySelector('.filtered-items')
-    let resultsParentElement = document.querySelector('.filters')
-    // console.log(results)
-    // console.log(element)
-    element.addEventListener('click', function (event) {
-        event.stopPropagation();
-        event.preventDefault();
+itemValues.map((element) => {
+  let results = document.querySelector(".filtered-items");
+  let resultsParentElement = document.querySelector(".filters");
+  element.addEventListener("click", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
 
-        resultsParentElement.style.opacity = "1"
+    resultsParentElement.style.opacity = "1";
 
-        if (element) {
-            results.innerHTML +=
-                `
+    if (element) {
+      results.innerHTML += `
             <div class="element-item">
                 <span>${element.innerHTML}</span>
                 <span class="material-icons close-icon">close</span>
             </div>
              `;
-            // console.log(element)
-        }
-
-        // const frontend = [...document.querySelectorAll('[value = frontend]')];
-        // const fullstack = [...document.querySelectorAll('[value = fullstack]')];
-        // const bodyElement = document.querySelector('body');
-        // console.log(bodyElement)
-        // console.log(frontend)
-
-        let btn = document.querySelectorAll('.category');
-        let arr = [...btn]
-
-        if (arr.value = 'frontend') {
-            console.log('frontend')
-        } else if (arr.value = 'fullstack') {
-            console.log('fullstack')
-        }
-
-        //hides selected item (selects parent element of the icon along with the description)
-        let closeButton = document.querySelectorAll('.close-icon');
-        let activeCloseButton = [...closeButton];
-
-        let i;
-        for (i = 0; i < activeCloseButton.length; i++) {
-            activeCloseButton[i].onclick = function () {
-                let div = this.parentElement;
-                div.style.display = "none"
-                // console.log(div)
-            }
-        };
-    })
-
-    //clears all selected elements on one click
-    const clearAll = document.querySelector('.clear');
-
-    clearAll.onclick = () => {
-        const myNode = resultsParentElement;
-        const myNodeText = results;
-        myNode.style.opacity = '0'
-        myNodeText.textContent = ''
     }
-})
 
+    let btn = document.querySelectorAll(".category");
+    let arr = [...btn];
+
+    //hides selected item (selects parent element of the icon along with the description)
+    let closeButton = document.querySelectorAll(".close-icon");
+    let activeCloseButton = [...closeButton];
+
+    let i;
+    for (i = 0; i < activeCloseButton.length; i++) {
+      activeCloseButton[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none";  
+      };
+    }
+  });
+
+  //clears all selected elements on one click
+  const clearAll = document.querySelector(".clear");
+
+  clearAll.onclick = () => {
+    const myNode = resultsParentElement;
+    let items = document.querySelectorAll(".item"); 
+    const myNodeText = results;
+    myNode.style.opacity = "0";
+    myNodeText.textContent = "";
+    items.forEach((item) => {
+        item.style.display = "block"
+    })
+  };
+});
+
+//filters elements depending on the value that has been clicked
+function filterClick() {
+  let buttons = document.querySelectorAll("button");
+  let items = document.querySelectorAll(".item");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const filter = e.target.dataset.filter;
+
+      items.forEach((item) => {
+        if (item.classList.contains(filter)) {
+          item.style.display = "block"; 
+          item.classList.add('active')
+        } 
+        else {
+          item.style.display = "none";
+          item.classList.remove('active')          
+        }
+      });
+    });
+  });
+}
+
+
+filterClick();
