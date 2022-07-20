b4547db7419345aff3688772ed956c9f342a0398
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2022-07-20 23:17:10 +0100

    applying filtering on the filtered items

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const parentContainer = document.querySelectorAll(".item");
 
 let resultsParentElement = document.querySelector(".filters");
 
+//loops through all buttons
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
 
@@ -18,37 +19,29 @@ buttons.forEach((button) => {
 
     selectedFilters.add(filter)
     let array = Array.from(selectedFilters);
-    //array.slice(-1).pop()
 
     results.innerHTML += `
     <div class="element-item ${filter}" data-filter="${filter}">
-        ${filter}
+        ${button.innerHTML}
     </div>
     `;
-    console.log(selectedFilters)
 
+    //loops through parent elements of the button elements (the whole container)
     parentContainer.forEach((item) => {
-
         if (item.classList.contains(filter)) {
           return;
 
         } else {
-          item.style.display = "none"
+          item.classList.add('inactive');
         }
-
-        // if (selectedFilters.has(filter)) {
-        //   console.log('test')
-        //   this.removeEventListener('click', (e))
-        // }
     })
 
     let closeButton = [...document.querySelectorAll(".element-item")];
-
+    //loops through filtered buttons
     closeButton.forEach((activeButton) => {
       activeButton.addEventListener('click', (e) => {
         e.preventDefault();
         e.stopPropagation();
-        console.log(selectedFilters)
 
         const filter2 = activeButton.dataset.filter;
 
@@ -56,15 +49,43 @@ buttons.forEach((button) => {
           console.log(filter2)
           activeButton.remove(filter2);    
           selectedFilters.delete(filter2); 
-          console.log(selectedFilters)
         }
 
         if(selectedFilters.size == 0) {
-          console.log(selectedFilters.size)
           document.querySelector(".filters").style.opacity = "0";
         }
+
+        parentContainer.forEach((item) => {
+
+
+            let array2 = Array.from(selectedFilters);
+            let classes = [... item.classList]
+            console.log(classes)
+            console.log(array2)
+
+  
+            const containsAll = array2.every(element => {
+              return classes.includes(element);
+            });
+  
+            //check if an array of all selected elements is included in to the array of the class elements
+            if(containsAll === true) {
+              item.classList.add('active');
+              item.classList.remove('inactive');
+            }
+            console.log(containsAll)
+  
+            if(array2.length === 0) {
+              console.log('length is 0')
+              item.classList.remove('active');
+              item.classList.remove('inactive');
+              return;
+            }
+        })
       })})
               
+
+
     //clears all selected elements on one click
     const clearAll = document.querySelector(".clear");
 
@@ -75,8 +96,9 @@ buttons.forEach((button) => {
       myNode.style.opacity = "0";
       myNodeText.textContent = "";
       items.forEach((item) => {
-        item.style.display = "flex";
         selectedFilters.clear();
+        item.classList.remove('active');
+        item.classList.remove('inactive');
       })
     };
   })
